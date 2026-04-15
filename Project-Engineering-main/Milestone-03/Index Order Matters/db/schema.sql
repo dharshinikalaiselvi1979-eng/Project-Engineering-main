7720b17fb3b48f26f92bdbc09f4435a85d807caf
DHARSHINI MAHENDRAN <dharshinikalaiselvi1979@gmail.com>
2026-04-15 17:49:09 +0530

    feat: added tenant_id and indexing for employees table

--- Project-Engineering-main/Milestone-03/Index Order Matters/db/schema.sql
+++ Project-Engineering-main/Milestone-03/Index Order Matters/db/schema.sql
@@ -1,11 +1,17 @@
--- Create employees table
+CREATE TABLE tenants (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     department VARCHAR(50),
     salary NUMERIC,
-    hire_date DATE
+    hire_date DATE,
+    tenant_id INT NOT NULL,
+    FOREIGN KEY (tenant_id) REFERENCES tenants(id)
 );
 
---composite index
 CREATE INDEX idx_salary_department ON employees(salary, department);
+CREATE INDEX idx_employees_tenant ON employees(tenant_id);
