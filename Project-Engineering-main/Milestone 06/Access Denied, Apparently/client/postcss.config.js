17333f1bcc80be8cea97e757ea39968427ee81fb
DHARSHINI MAHENDRAN <dharshinikalaiselvi1979@gmail.com>
2026-04-25 23:18:38 +0530

    kjik

--- Project-Engineering-main/Milestone 06/Access Denied, Apparently/client/postcss.config.js
+++ Project-Engineering-main/Milestone 06/Access Denied, Apparently/client/postcss.config.js
@@ -4,3 +4,4 @@ export default {
       autoprefixer: {},
     },
   }
+// This is a PostCSS configuration file that specifies the use of Tailwind CSS and Autoprefixer as PostCSS plugins. Tailwind CSS is a utility-first CSS framework, while Autoprefixer automatically adds vendor prefixes to CSS rules for better browser compatibility.
